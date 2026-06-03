407805e9137e658aa19a4a452e24a0ec2b68c4f0
Iresh Ekanayaka <rovtyhq@gmail.com>
2026-06-03 22:49:41 +0530

    location fixed

--- app/location/page.tsx
+++ app/location/page.tsx
@@ -78,7 +78,7 @@ export default function LocationPage() {
             {/* Map Placeholder */}
             <div className="relative h-80 lg:h-[400px] rounded-2xl overflow-hidden shadow-lg bg-gray-200">
               <iframe
-                src={`https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3961.2!2d${siteConfig.coordinates.lng}!3d${siteConfig.coordinates.lat}!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zNsKwNTUnMzcuNiJOIDc5wrA1MSc0MC4zIkU!5e0!3m2!1sen!2s!4v1`}
+                src={`https://maps.google.com/maps?q=${siteConfig.coordinates.lat},${siteConfig.coordinates.lng}&z=17&output=embed`}
                 width="100%"
                 height="100%"
                 style={{ border: 0 }}
